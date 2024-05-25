dfee3efec5a918317d2c0fa5521462281faeb274
Bonevy BEBY <bonevybeby@gmail.com>
2024-05-25 15:58:35 +0200

    added:Form and controller

--- src/main/java/com/formationkilo/javafxjdbcmaven/MainController.java
+++ src/main/java/com/formationkilo/javafxjdbcmaven/MainController.java
@@ -1,14 +1,26 @@
 package com.formationkilo.javafxjdbcmaven;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
 public class MainController {
     @FXML
-    private Label welcomeText;
+    private TextField emailtxt;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
+    private PasswordField passwordtxt;
+   public void getLogin(ActionEvent e){
+       String email=emailtxt.getText();
+       String password=passwordtxt.getText();
+       String params=email+" "+password;
+       Alert alert=new Alert(Alert.AlertType.INFORMATION);
+       alert.setTitle("Message");
+       alert.setContentText("hello User");
+
+       alert.showAndWait();
+   }
 }
